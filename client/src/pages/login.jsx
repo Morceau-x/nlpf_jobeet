3e497cf595330d060be3729c434a7cf40cb66186
Esteban Casanas <esteban.ca19@gmail.com>
2019-09-14 14:05:09 +0200

    Added bootstrap module to do a non functional login form for now

---
+++ client/src/pages/login.jsx
@@ -0,0 +1,14 @@
+import LoginForm from '../components/LoginForm'
+import React, {Component} from "react"
+
+class Login extends Component {
+ render() {
+     return (
+         <div>
+             <LoginForm></LoginForm>
+         </div>
+     )
+ }
+}
+
+export default Login
