2be9403808a137ae368be8043bc23e0b50fd8181
Maxime L'Hermite <maxime.lhermite@epita.fr>
2019-09-18 14:20:28 +0200

    Added Logo

--- frontend/src/layouts/Header.js
+++ frontend/src/layouts/Header.js
@@ -1,101 +1,113 @@
-import React, { Component } from "react";
+import React, {Component} from "react";
 import PropTypes from "prop-types";
-import { connect } from "react-redux";
-import { logoutUser } from "../actions/authActions";
+import {connect} from "react-redux";
+import {logoutUser} from "../actions/authActions";
 import {
-  Collapse,
-  Navbar,
-  NavbarToggler,
-  NavbarBrand,
-  Nav,
-  NavItem,
-  NavLink,
-  UncontrolledDropdown,
-  DropdownToggle,
-  DropdownMenu,
-  DropdownItem
+    Collapse,
+    Navbar,
+    NavbarToggler,
+    NavbarBrand,
+    Nav,
+    NavItem,
+    NavLink,
+    UncontrolledDropdown,
+    DropdownToggle,
+    DropdownMenu,
+    DropdownItem
 } from "reactstrap";
 
 class Header extends Component {
-  onLogoutClick(e) {
-    e.preventDefault();
-    this.props.logoutUser();
-  }
-  constructor(props) {
-    super(props);
+    onLogoutClick(e) {
+        e.preventDefault();
+        this.props.logoutUser();
+    }
 
-    this.toggle = this.toggle.bind(this);
-    this.state = {
-      isOpen: false
-    };
-  }
-  toggle() {
-    this.setState({
-      isOpen: !this.state.isOpen
-    });
-  }
-  render() {
-    const { isAuthenticated, user } = this.props.auth;
-    const authLinks = (
-      <Nav className="ml-auto" navbar>
-        <UncontrolledDropdown nav inNavbar>
-          <DropdownToggle nav caret>
-            Hello {user.name}
-          </DropdownToggle>
-          <DropdownMenu right>
-            <DropdownItem>Profile</DropdownItem>
-            <DropdownItem divider />
-            <DropdownItem onClick={this.onLogoutClick.bind(this)}>
-              Logout
-            </DropdownItem>
-          </DropdownMenu>
-        </UncontrolledDropdown>
-      </Nav>
-    );
-    const guestLinks = (
-      <Nav className="ml-auto" navbar>
-        <NavItem>
-          <NavLink href="/register">Register</NavLink>
-        </NavItem>
-        <NavItem>
-          <NavLink href="/login">Login</NavLink>
-        </NavItem>
-      </Nav>
-    );
-    const candidatNav = (
-      <Nav className="ml-auto" navbar>
-        <NavItem>
-          <NavLink href="/dashboard">Offres</NavLink>
-        </NavItem>
-        <NavItem>
-          <NavLink href="/notifications">Notifications</NavLink>
-        </NavItem>
-      </Nav>
-    );
-    return (
-      <div className="mb-2">
-        <Navbar color="white" className="navbar-mern" light expand="md">
-          <NavbarBrand href="/">Jobeet</NavbarBrand>
-          <NavbarToggler onClick={this.toggle} />
-          {isAuthenticated ? candidatNav : null}
-          <Collapse isOpen={this.state.isOpen} navbar>
-            {isAuthenticated ? authLinks : guestLinks}
-          </Collapse>
-        </Navbar>
-      </div>
-    );
-  }
+    constructor(props) {
+        super(props);
+
+        this.toggle = this.toggle.bind(this);
+        this.state = {
+            isOpen: false
+        };
+    }
+
+    toggle() {
+        this.setState({
+            isOpen: !this.state.isOpen
+        });
+    }
+
+    render() {
+        const {isAuthenticated, user} = this.props.auth;
+        const authLinks = (
+            <Nav className="ml-auto" navbar>
+                <UncontrolledDropdown nav inNavbar>
+                    <DropdownToggle nav caret>
+                        Hello {user.name}
+                    </DropdownToggle>
+                    <DropdownMenu right>
+                        <DropdownItem>Profile</DropdownItem>
+                        <DropdownItem divider/>
+                        <DropdownItem onClick={this.onLogoutClick.bind(this)}>
+                            Logout
+                        </DropdownItem>
+                    </DropdownMenu>
+                </UncontrolledDropdown>
+            </Nav>
+        );
+        const guestLinks = (
+            <Nav className="ml-auto" navbar>
+                <NavItem>
+                    <NavLink href="/register">Register</NavLink>
+                </NavItem>
+                <NavItem>
+                    <NavLink href="/login">Login</NavLink>
+                </NavItem>
+            </Nav>
+        );
+        const candidatNav = (
+            <Nav className="ml-auto" navbar>
+                <NavItem>
+                    <NavLink href="/dashboard">Offres</NavLink>
+                </NavItem>
+                <NavItem>
+                    <NavLink href="/notifications">Notifications</NavLink>
+                </NavItem>
+            </Nav>
+        );
+        return (
+            <div className="mb-2">
+                <Navbar color="white" className="navbar-mern" light expand="md">
+                    <NavbarBrand href="/" className="d-inline pr-2 pl-2">
+                        <img
+                            alt=""
+                            src="favicon.ico"
+                            width="40"
+                            height="40"
+                            className="d-inline-block mr-2"
+                        />
+                        <h4 className="d-inline-block mr-2" >Jobeet</h4>
+                    </NavbarBrand>
+                    <NavbarToggler onClick={this.toggle}/>
+                    {isAuthenticated ? candidatNav : null}
+                    <Collapse isOpen={this.state.isOpen} navbar>
+                        {isAuthenticated ? authLinks : guestLinks}
+                    </Collapse>
+                </Navbar>
+            </div>
+        );
+    }
 }
 
 Header.propTypes = {
-  logoutUser: PropTypes.func.isRequired,
-  auth: PropTypes.object.isRequired
+    logoutUser: PropTypes.func.isRequired,
+    auth: PropTypes.object.isRequired
 };
 
 const mapStateToProps = state => ({
-  auth: state.auth
+    auth: state.auth
 });
 export default connect(
-  mapStateToProps,
-  { logoutUser }
+    mapStateToProps,
+    {logoutUser}
 )(Header);
