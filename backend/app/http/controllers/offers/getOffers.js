7e5a468e33c7f32e619cfe465fbe94d709c06e86
Maxime L'Hermite <maxime.lhermite@epita.fr>
2019-10-01 12:21:58 +0200

    Added function to get offers

--- backend/app/http/controllers/offers/getOffers.js
+++ backend/app/http/controllers/offers/getOffers.js
@@ -1,11 +1,20 @@
 const Offers = require("../../../models/Offers");
 
-
 exports.getAllOffers = (req, res) => {
  Offers.find({}).then(offer => {
   if (!offer) {
    return res.status(404).json("skills not found");
   }
+  return res.json(offer)
+ })
+};
+
+exports.getCompanyOffers = (req, res) => {
+ Offers.find({}).then(offer => {
+  if (!offer) {
+   return res.status(404).json("Offer not found");
+  }
+  offer.filter(offer => offer.company === req.query.company);
   return res.json(offer)
  })
 };
