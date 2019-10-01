5d28a0559c10e4e425ebc90550bf25a224a3226a
Maxime L'Hermite <maxime.lhermite@epita.fr>
2019-10-01 12:20:16 +0200

    Handled permissions in Offers

--- frontend/src/components/company/Offers.js
+++ frontend/src/components/company/Offers.js
@@ -1,4 +1,6 @@
 import React, {Component} from "react";
+import {Link, Route} from "react-router-dom";
+import axios from "axios";
 
 import {
     Card,
@@ -12,32 +14,44 @@ import {
     FormFeedback,
     Input
 } from "reactstrap";
-import Register from "../auth/Register";
-import {Link, Route} from "react-router-dom";
-import $ from 'jquery';
+import PropTypes from "prop-types";
+import {connect} from "react-redux";
 
 
 class Offers extends Component {
-    constructor() {
-        super();
+    constructor(props) {
+        super(props);
         this.state = {
-            offers: [{
-                name: "this a test offer",
-                short: "This is the description of a test offer",
-                id: ""
-            }, {}, {}, {}, {}, {}]
+            offers: []
         };
     }
 
-    makeCard(data) {
-        return (
-            <Card id={data.name} className="mb-5 btn btn-outline-dark text-left">
-                <CardBody>
-                    <h5 className="card-title">{data.name}</h5>
-                    {data.short}
-                </CardBody>
-            </Card>
+    isRecruiter() {
+        const {isAuthenticated, user} = this.props.auth;
 
+        return isAuthenticated && user.role !== 1
+    }
+
+    componentWillMount() {
+
+        axios
+            .get('/offers/company')
+            .then(response => {
+                this.setState({offers: response.data});
+                console.log(response.data);
+            });
+    }
+
+    static makeCard(data) {
+        return (
+            <div className="col-lg-4 col-md-6 mb-4">
+                <Card id={data._id} className="btn btn-outline-dark text-left">
+                    <CardBody>
+                        <h5 className="card-title">{data.offerName}</h5>
+                        {data.shortDesc}
+                    </CardBody>
+                </Card>
+            </div>
         )
     }
 
@@ -47,17 +61,20 @@ class Offers extends Component {
 
         for (let i = 0; i < this.state.offers.length; i++) {
             let data = this.state.offers[i];
-            items.push(this.makeCard(data))
+            items.push(Offers.makeCard(data))
         }
 
         return (
             <Card>
                 <CardBody>
-                    <Link to='/offer/create'>
-                        <button type="button" className="btn btn-warning float-right">Create offer</button>
-                    </Link>
+                    {this.isRecruiter() ?
+                        <Link to='/offer/create'>
+                            <button type="button" className="btn btn-warning float-right">Create offer</button>
+                        </Link> : <p></p>
+                    }
+
                     <h4 className="card-title mb-5">Offers of the company</h4>
-                    <div>
+                    <div className="row">
                         {items}
                     </div>
                 </CardBody>
@@ -66,4 +83,14 @@ class Offers extends Component {
     }
 }
 
-export default Offers;
+Offers.propTypes = {
+    auth: PropTypes.object.isRequired
+};
+const mapStateToProps = state => ({
+    auth: state.auth
+});
+
+export default connect(
+    mapStateToProps,
+    {}
+)(Offers);
