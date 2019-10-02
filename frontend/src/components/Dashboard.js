26aa37ca538835a4ecc6f0e217d74d3abd0934bf
Esteban Casanas <esteban.ca19@gmail.com>
2019-10-02 16:25:05 +0200

    Dashboard: added pagination

--- frontend/src/components/Dashboard.js
+++ frontend/src/components/Dashboard.js
@@ -3,6 +3,7 @@ import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import OfferOverview from "./OfferOverview"
 import axios from "axios";
+import Pagination from "react-js-pagination";
 
 
 class Dashboard extends Component {
@@ -10,7 +11,10 @@ class Dashboard extends Component {
   constructor() {
     super();
     this.state = {
-      offersList: []
+      offersList: [],
+      displayedList: [],
+      itemsCountPerPage: 10,
+      activePage: 1
     };
   }
 
@@ -23,20 +27,36 @@ class Dashboard extends Component {
       .then(response => (
         this.setState({
           offersList: response.data
-        })
+        }),
+        this.handlePageChange(1)
       ))
+  }
+
+  handlePageChange = (pageNumber) => {
+    console.log(pageNumber)
+    this.setState({activePage: pageNumber});
+    let startIndex = this.state.itemsCountPerPage * (pageNumber - 1);
+    let buffArray = [].concat(this.state.offersList);
+    this.setState({displayedList: buffArray.splice(startIndex, this.state.itemsCountPerPage)});
   }
 
   render() {
     return (
       <div className="container">
         <div className="row">
-          {this.state.offersList.map((item, index) => (
+          {this.state.displayedList.map((item, index) => (
             <div className="col-4" key={index} item={item}>
               <OfferOverview offer={item} />
             </div>
           ))}
         </div>
+        <Pagination
+          activePage={this.state.activePage}
+          itemsCountPerPage={this.state.itemsCountPerPage}
+          totalItemsCount={this.state.offersList.length}
+          pageRangeDisplayed={3}
+          onChange={this.handlePageChange}
+        />
       </div>
     );
   }
