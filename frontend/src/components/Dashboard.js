4cad5d1fb7f31bf95655761bce2239300b6afaa9
Esteban Casanas <esteban.ca19@gmail.com>
2019-10-07 12:20:02 +0200

    Dashboard: added number of entries, about to add filter

--- frontend/src/components/Dashboard.js
+++ frontend/src/components/Dashboard.js
@@ -13,9 +13,10 @@ class Dashboard extends Component {
     this.state = {
       offersList: [],
       displayedList: [],
-      itemsCountPerPage: 10,
+      itemsCountPerPage: 6,
       activePage: 1
     };
+    this.setItemCount = this.setItemCount.bind(this);
   }
 
   componentWillMount() {
@@ -32,18 +33,37 @@ class Dashboard extends Component {
       ))
   }
 
+  setItemCount = (e) => {
+    console.log("value", Number(e.target.value))
+    this.setState({ itemsCountPerPage: Number(e.target.value) })
+    let startIndex = Number(e.target.value) * (this.state.activePage - 1);
+    let buffArray = [].concat(this.state.offersList);
+    this.setState({ displayedList: buffArray.splice(startIndex, Number(e.target.value)) });
+    console.log("After", this.state)
+
+  }
+
   handlePageChange = (pageNumber) => {
     console.log(pageNumber)
-    this.setState({activePage: pageNumber});
+    this.setState({ activePage: pageNumber });
     let startIndex = this.state.itemsCountPerPage * (pageNumber - 1);
     let buffArray = [].concat(this.state.offersList);
-    this.setState({displayedList: buffArray.splice(startIndex, this.state.itemsCountPerPage)});
+    this.setState({ displayedList: buffArray.splice(startIndex, this.state.itemsCountPerPage) });
   }
 
   render() {
     return (
       <div className="container">
-        <div className="row">
+        <div className="dropdown">
+          Show
+           <select className="ml-1 mr-1" id="lang" onChange={this.setItemCount} value={this.state.itemsCountPerPage}>
+            <option value="6">6</option>
+            <option value="12">12</option>
+            <option value="24">24</option>
+          </select>
+          entries
+        </div>
+        <div className="row mt-2">
           {this.state.displayedList.map((item, index) => (
             <div className="col-4" key={index} item={item}>
               <OfferOverview offer={item} />
